bc7deb5d97d8e3926602c69839196c0ff310ecfb
Martin Mercer <martinmercer1@gmail.com>
2019-03-20 10:37:32 -0600

    now I'm done.

--- lib/open_mic.rb
+++ lib/open_mic.rb
@@ -1,11 +1,35 @@
 class OpenMic
 
   attr_reader :location,
-              :date
+              :date,
+              :performers
 
   def initialize(attributes = Hash.new)
     @location = attributes[:location]
     @date = attributes[:date]
+    @performers = []
   end
-  
+
+  def welcome(user)
+    @performers.push(user)
+  end
+
+  def all_jokes
+    jokes = []
+    @performers.map do |performer|
+      performer.jokes.select do |j|
+        jokes.push(j)
+      end
+    end
+    jokes
+  end
+
+  def repeated_jokes?
+    if all_jokes.uniq.length == all_jokes.length
+      false
+    else
+      true
+    end
+  end
+
 end
--- test/open_mic_test.rb
+++ test/open_mic_test.rb
@@ -22,4 +22,52 @@ class OpenMicTest < MiniTest::Test
     assert_equal "11-20-18", open_mic.date
   end
 
+  def test_it_initiates_with_no_performers
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    assert_equal [], open_mic.performers
+  end
+
+  def test_it_can_welcome_performers
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    open_mic.welcome(sal)
+    open_mic.welcome(ali)
+
+    assert_equal [sal, ali], open_mic.performers
+  end
+
+  def test_it_can_show_a_collection_of_all_jokes_from_all_performers
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    open_mic.welcome(sal)
+    open_mic.welcome(ali)
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    ali.learn(joke_1)
+    ali.learn(joke_2)
+    ali.tell(sal, joke_1)
+
+    assert_equal [joke_1, joke_1, joke_2], open_mic.all_jokes
+  end
+
+  def test_it_can_show_repeated_jokes
+    open_mic = OpenMic.new({location: "Comedy Works", date: "11-20-18"})
+    sal = User.new("Sal")
+    ali = User.new("Ali")
+    open_mic.welcome(sal)
+    open_mic.welcome(ali)
+    joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
+
+    ali.learn(joke_1)
+    ali.learn(joke_2)
+    refute open_mic.repeated_jokes?
+
+    ali.tell(sal, joke_1)
+    assert open_mic.repeated_jokes?
+  end
+
 end
